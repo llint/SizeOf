705fc80758e48f2af5cbc6a1fafc69944771fab6
Lin Luo <l.luo-pi@outlook.com>
2018-06-08 19:16:17 -0700

    Initial Commit

---
+++ SizeOf/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  SizeOf
+//
+//  Created by Lin Luo on 6/8/18.
+//  Copyright © 2018 Linfinity. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
